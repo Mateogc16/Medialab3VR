81b8897f25263eae155a7553f48d4fe13b6f67c9
Juampa09 <juanaristi.david9@gmail.com>
2026-05-09 15:15:00 -0500

    Canvas set up
    Se agregaron los canvas a todos los objetos interactuables menos las máscaras de la zona 2 y el tutorial al inicio de la primera zona

--- Medialab3VR/Assets/scripts/ActivacionCanvas.cs
+++ Medialab3VR/Assets/scripts/ActivacionCanvas.cs
@@ -1,10 +1,15 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class ShowCanvasOnGrab : MonoBehaviour
 {
     public GameObject canvas;
+    public Image image;
+
+    public float fadeDuration = 0.5f;
 
     private XRGrabInteractable grabInteractable;
 
@@ -25,13 +30,54 @@ public class ShowCanvasOnGrab : MonoBehaviour
         grabInteractable.selectExited.RemoveListener(OnRelease);
     }
 
+    void Start()
+    {
+        Color color = image.color;
+        color.a = 0f;
+        image.color = color;
+
+        canvas.SetActive(false);
+    }
+
     void OnGrab(SelectEnterEventArgs args)
     {
+        StopAllCoroutines();
+
         canvas.SetActive(true);
+        StartCoroutine(FadeImage(0f, 1f));
     }
 
     void OnRelease(SelectExitEventArgs args)
     {
+        StopAllCoroutines();
+
+        StartCoroutine(FadeOutAndDisable());
+    }
+
+    IEnumerator FadeImage(float start, float end)
+    {
+        float elapsed = 0f;
+
+        Color color = image.color;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+
+            color.a = Mathf.Lerp(start, end, elapsed / fadeDuration);
+            image.color = color;
+
+            yield return null;
+        }
+
+        color.a = end;
+        image.color = color;
+    }
+
+    IEnumerator FadeOutAndDisable()
+    {
+        yield return StartCoroutine(FadeImage(1f, 0f));
+
         canvas.SetActive(false);
     }
 }
